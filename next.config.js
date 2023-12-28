812b2e0f8afe5ae322c74551ff20b846e2ea9bd5
harunjeylan <harunjeylanwako@gmail.com>
2023-12-28 08:55:10 +0000

    non change

--- next.config.js
+++ next.config.js
@@ -19,10 +19,9 @@ const nextConfig = {
     AWS_SES_PASSWORD: "<Update here AWS SES PASSWORD>",
     CLOUD_NAME: "dcwbze0dz",
     UPLOAD_PRESETS: "fast-lms",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dcwbze0dz/image/upload",
-    CLOUDINARY_VIDEO_URL:
-      "https://api.cloudinary.com/v1_1/dcwbze0dz/video/upload",
-    CLOUDINARY_ZIP_URL: "https://api.cloudinary.com/v1_1/dcwbze0dz/raw/upload",
+    CLOUDINARY_URL: `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/image/upload`,
+    CLOUDINARY_VIDEO_URL: `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/video/upload`,
+    CLOUDINARY_ZIP_URL: `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/raw/upload`,
     STRIPE_SECRET_KEY: "sk_test_51OMS4zCPECoxMYA9bjIBMQwKm8d56VZSnUSfn3wBCiyFPl2asc9xJ6cD9c7jVq9NjHVRMmhswW8ssIG7yjAWi4FO0051k1tARR",
     STRIPE_PUBLISHABLE_KEY: "pk_test_51OMS4zCPECoxMYA9jO56XHxbEv1SWmui2pmo0jYgnfqks9bxVFPNH8aF2bg9TCjiq3L5qc1FzuGZ37QkL4WcseQw00v6iQrEJa",
   },
